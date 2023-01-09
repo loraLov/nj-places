85d2bea155af8f85097d97c805a96ae193718f1e
loraLov <lauzhynskaya@gmail.com>
2023-01-09 18:01:12 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import {data} from './data';
 import './App.css';
 
 function App() {
+ 
+
+
+  const [place, setPlace] = useState(0);
+  const {id, name, image, description, location, showMore} = data[place];
+  const[showText, setShowText] = useState(false);
+  const showTextClick = (place) => {
+    place.showMore=!place.showMore;
+    setShowText (!showText)
+  }
+  
+
+  const previousPlace = () =>{
+    setPlace((place =>{
+      place --;
+      if(place < 0){
+        return data.length -1;
+      }
+      return place
+    }))
+  }
+
+  const nextPlace = () => {
+    setPlace ((place =>{
+      place ++;
+      if (place > data.length -1){
+        place = 0;
+      }
+      return place;
+    }))
+  }
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+
+      <div className="container">
+        <h1>{data.length} best places to visit in New Jersey</h1>
+      </div>
+      <div className='container'>
+        <h2>{id} - {name}</h2> 
+      </div>
+      <div className = 'container'>
+        <p className = "text">{showMore? description:description.substring(0,200) + "..."} <button className="newButton" onClick = {() => showTextClick(place)}>{showMore? "Show less":"Show more"}</button></p>
+      </div>
+
+      <div className = 'container'>
+        <img src = {image} width="600px" alt = "place"/>
+      </div>
+
+      <div className = 'container'>
+       <h5>{location}</h5>
+      </div>
+
+    <div className = "container">
+      <button className="btn" onClick = {previousPlace}>Prevoius</button>
+      <button className="btn"  onClick = {nextPlace}>Next</button>
+      </div>
+
+
+          </div>
+  
   );
 }
 
