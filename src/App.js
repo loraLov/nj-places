b43c620d8b37c81227d34cf60d91cfdf7161e6f4
loraLov <lauzhynskaya@gmail.com>
2023-01-10 11:40:11 -0500

    your message

--- src/App.js
+++ src/App.js
@@ -7,47 +7,62 @@ function App() {
 
 
   const [place, setPlace] = useState(0);
-  const {id, name, image, description, location, showMore} = data[place];
-  const[showText, setShowText] = useState(false);
-  const showTextClick = (place) => {
-    place.showMore=!place.showMore;
-    setShowText (!showText)
-  }
+  const {id, name, image, description, location} = data[place];
+  const [showText, setShowText] = useState(false);
+
+
+const [full, setFull] = useState (data);
+const removePlace = (id) => {
+  nextPlace();
+  let newData = full.filter( item => item.id!==id);
+  console.log(newData);
+  setFull(newData);
+ 
+  
+}
+
+
+  
   
 
   const previousPlace = () =>{
     setPlace((place =>{
       place --;
+      setShowText(false);
+    
       if(place < 0){
         return data.length -1;
       }
       return place
     }))
+  
   }
 
   const nextPlace = () => {
     setPlace ((place =>{
       place ++;
+      setShowText(false);
+      
       if (place > data.length -1){
         place = 0;
       }
       return place;
     }))
   }
-  
+
 
 
   return (
     <div>
 
       <div className="container">
-        <h1>{data.length} best places to visit in New Jersey</h1>
+         <h1>{data.length} best places to visit in New Jersey</h1>
       </div>
       <div className='container'>
         <h2>{id} - {name}</h2> 
       </div>
       <div className = 'container'>
-        <p className = "text">{showMore? description:description.substring(0,200) + "..."} <button className="newButton" onClick = {() => showTextClick(place)}>{showMore? "Show less":"Show more"}</button></p>
+        <p className = "text">{showText? description:description.substring(0,200) + "..."} <button className="newButton" onClick = {() => setShowText(!showText)}>{showText? "Show less":"Show more"}</button></p>
       </div>
 
       <div className = 'container'>
@@ -63,6 +78,10 @@ function App() {
       <button className="btn"  onClick = {nextPlace}>Next</button>
       </div>
 
+
+<div className = 'container'>
+  <button onClick = {() => removePlace(id)}>Remove as visited</button>
+</div>
 
           </div>
   
--- src/data.js
+++ src/data.js
@@ -4,8 +4,8 @@ export const data = [
         name: "Princeton University",
         image: "https://images.pexels.com/photos/14495738/pexels-photo-14495738.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         description: "Princeton University is a private research university in Princeton, New Jersey. Founded in 1746 in Elizabeth as the College of New Jersey, Princeton is the fourth-oldest institution of higher education in the United States and one of the nine colonial colleges chartered before the American Revolution. It is one of the highest-ranked universities in the world. The institution moved to Newark in 1747, and then to the current site nine years later. It officially became a university in 1896 and was subsequently renamed Princeton University. It is a member of the Ivy League.",
-        location: "Location: 171 Broadmead St, Princeton, NJ 08540",
-        showMore:false
+        location: "Location: 171 Broadmead St, Princeton, NJ 08540"
+        
         
     },
 
@@ -14,8 +14,8 @@ export const data = [
         name: "Empty Sky 9/11 Memorial",
         image: "https://images.pexels.com/photos/747100/pexels-photo-747100.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         description: "The Empty Sky Memorial honors the 749 people from New Jersey who lost their lives during the September 11th attacks. Their names are engraved into two tall silver walls that make up the memorial. When visitors walk down the granite path between the walls, it will lead them to where the World Trade Center buildings once stood across the Hudson River. Located near the Central Railroad of New Jersey Terminal, the memorial “remembers those lost while simply and powerfully connecting New Jersey to Ground Zero.”",
-        location: "Location: 1 Audrey Zapp Dr, Jersey City, NJ 07305",
-        showMore:false
+        location: "Location: 1 Audrey Zapp Dr, Jersey City, NJ 07305"
+        
         
     },
 
@@ -24,8 +24,8 @@ export const data = [
     name: "Wild Safari Drive-Thru Adventure",
     image: "https://images.pexels.com/photos/3529692/pexels-photo-3529692.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     description: "Boasting 1,200 animals from six continents including elephants, rhinos, tigers, giraffes and lions, Wild Safari Drive-Thru Adventure takes explorers on an exhilarating, interactive animal adventure. Guests can explore the most interesting and exotic creatures at a 350-acre preserve in Jackson, New Jersey. The journey takes about 1.5 - 3 hours and is done in your own vehicle!",
-    location: "Location: 1 Six Flags Blvd, Jackson, NJ 08527",
-    showMore:false
+    location: "Location: 1 Six Flags Blvd, Jackson, NJ 08527"
+    
     
 },
 
@@ -34,8 +34,8 @@ export const data = [
     name: "Six Flags Great Adventure",
     image: "https://townsquare.media/site/396/files/2021/06/Rollercoaster.jpg?w=1200&h=0&zc=1&s=0&a=t&q=89",
     description: "Six Flags Great Adventure is an amusement park located in Jackson, New Jersey. Owned and operated by Six Flags, the park complex is situated between New York City and Philadelphia and includes a water park named Hurricane Harbor. It first opened to the public as simply Great Adventure in 1974 under the direction of restaurateur Warner LeRoy. Six Flags acquired the park in 1977. At 510 acres (210 ha), it is the second-largest theme park in the world following Disney's Animal Kingdom.",
-    location: "Location: 1 Six Flags Blvd, Jackson, NJ 08527",
-    showMore:false
+    location: "Location: 1 Six Flags Blvd, Jackson, NJ 08527"
+    
     
 },
 
@@ -44,8 +44,8 @@ export const data = [
     name: "DreamWorks Water Park",
     image: "https://www.wwashow.org/wp-content/uploads/2020/10/AmericanDream_1.png",
     description: "DreamWorks Water Park has the largest selection of water rides and biggest indoor wave pool worldwide and features record-breaking rides, luxury cabanas, and an 81°F tropical climate all year long. Just 5 minutes from NYC!",
-    location: "Location: 1 American Dream Way, East Rutherford, NJ 07073",
-    showMore:false
+    location: "Location: 1 American Dream Way, East Rutherford, NJ 07073"
+    
     
 },
 
